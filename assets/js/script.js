02644217c46c724dfe49499e6811bb872fe1ec5f
C316 <royer.seguracalderon@gmail.com>
2021-08-02 10:08:37 +0000

    include reset button at end of quiz and fix color background matching right or wrong answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,10 @@ const answerButtonsElement = document.getElementById('answer-buttons');
 let randomQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+});
 
 function startGame() {
     startButton.classList.add('hide');
@@ -36,6 +40,7 @@ function showQuestion(question) {
 };
 
 function resetState() {
+    clearStatusClass(document.body);
     nextButton.classList.add('hide');
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
@@ -51,6 +56,12 @@ function selectAnswer(e) {
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+    if (randomQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'Restart';
+        startButton.classList.remove('hide');
+    }
 };
 
 function setStatusClass(element, correct) {
