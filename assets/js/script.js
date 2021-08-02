f5b988aa4c355008fd39710c5f545634b09ecd29
C316 <royer.seguracalderon@gmail.com>
2021-08-02 09:49:19 +0000

    add functions that determine if chosen answer is correct or incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
@@ -8,7 +9,6 @@ let randomQuestions, currentQuestionIndex;
 startButton.addEventListener('click', startGame);
 
 function startGame() {
-    console.log('Started');
     startButton.classList.add('hide');
     randomQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
@@ -17,6 +17,7 @@ function startGame() {
 };
 
 function setNextQuestion() {
+    resetState()
     showQuestion(randomQuestions[currentQuestionIndex]);
 };
 
@@ -34,8 +35,36 @@ function showQuestion(question) {
     })
 };
 
-function selectAnswer() {
+function resetState() {
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+};
 
+/* Checks if selected button is part of the correct dataset */
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+};
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+};
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 };
 
 /* Game questions with 4 total choices */
